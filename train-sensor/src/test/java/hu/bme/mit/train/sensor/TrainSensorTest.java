213dd461e4c329f00a1878724378707017ef5ae5
Bozó Bálint Vid <bozo.balint.vid@gmail.com>
2021-03-08 03:36:48 +0100

    fix 10

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,22 +3,29 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-//import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.*;
 
 public class TrainSensorTest {
-	int test;
+	TrainController controller;
+	TrainSensor sensor;
+	TrainUser user;
 
     @Before
     public void before() {
-    	test = 2;
+    	
 		
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
-		test++;
-		Assert.assertEquals(3, test);
+		sensor.overrideSpeedLimit(50);
+		sensor.overrideSpeedLimit(5440);
+		Assert.assertEquals(50, sensor.getSpeedLimit());
 
 
     }
