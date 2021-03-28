0a96b096bb58adaf2224478bf3ce897d09e7447c
Bozó Bálint Vid <bozo.balint.vid@gmail.com>
2021-03-29 00:19:53 +0200

    test created

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,26 +1,56 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
-	int test;
+
+    TrainUser user;
+    TrainSensor sensor;
+    TrainController controller;
 
     @Before
     public void before() {
-    	test = 1;
-		
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-		test++;
-		Assert.assertEquals(2, test);
-
-
+    @DisplayName("Setting a negative value for speedlimit")
+    public void AbsoluteLimitNegative() {
+        sensor.overrideSpeedLimit(-1);
+        Mockito.verify(user, times(1)).setAlarmState(true);
     }
+
+    @Test
+    @DisplayName("Setting a value over 500")
+    public void AbsoluteLimitOver() {
+        sensor.overrideSpeedLimit(501);
+        Mockito.verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    @DisplayName("Setting a value under half")
+    public void LessThanHalfSpeedLimit() {
+        sensor.overrideSpeedLimit(150);
+        sensor.overrideSpeedLimit(50);
+        Mockito.verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    @DisplayName("Setting a value right, check alarm stay false")
+    public void AlarmFalseCheck() {
+        when(controller.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(120);
+        Mockito.verify(user, never()).setAlarmState(true);
+    }
+
 }
